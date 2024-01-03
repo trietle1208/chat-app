cc313fcfe53c27c818610ed735fe9bdbf099a3c6
TrietLe <lmtriet1208@gmail.com>
2024-01-03 22:11:05 +0700

    install & setup socket io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
-const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3000;
-
-const app = express();
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
 
-app.use(express.static(publicPath));
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicPath, '/index.html'));
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('a user was disconnected');
+  })
+});
+
+server.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
 });
